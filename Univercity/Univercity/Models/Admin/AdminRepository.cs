6fafe80a8a53171c754fc0f7bdd545c9986be535
masoomeshams <fmezadi9304265430@gmail.com>
2019-06-11 14:14:51 -0700

    ایجاد یک پوشه برای ادمین دربخش مدل که شامل اینترفیس و ریپوزیتوری ادمین است

---
+++ Univercity/Univercity/Models/Admin/AdminRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Univercity.Models;
+
+namespace Univercity
+{
+    public class AdminRepository : IAdmin
+    {
+        univercityEntities2 db = new univercityEntities2();
+        private tbl_teacher_presonal_information _teacher_p_i;
+
+        public string code_national_teacher
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Deleteteacher(tbl_teacher_presonal_information _teacher_p_i)
+        {
+            db.tbl_teacher_presonal_information.Remove(_teacher_p_i);
+            return db.SaveChanges();
+        }
+
+        public tbl_teacher_presonal_information FindTeacher(string code_national_teacher)
+        {
+            tbl_teacher_presonal_information teacher = db.tbl_teacher_presonal_information.Where(c => c.code_national_teacher == code_national_teacher).SingleOrDefault();
+            return teacher;
+
+        }
+
+        public List<tbl_teacher_presonal_information> FindTeachers(string code_national_teacher)
+        {
+            List<tbl_teacher_presonal_information> teachers = db.tbl_teacher_presonal_information.Where(c => c.code_national_teacher == code_national_teacher).ToList();
+            return teachers;
+        }
+
+        public int insertlistteacher(IEnumerable<tbl_teacher_presonal_information> liteacher)
+        {
+            db.tbl_teacher_presonal_information.AddRange(liteacher);
+            return db.SaveChanges();
+        }
+
+        public int Insertteacher(tbl_teacher_presonal_information _teacher_p_i)
+        {
+            db.tbl_teacher_presonal_information.Add(_teacher_p_i);
+            return db.SaveChanges();
+            
+        }
+
+        public int removelistteacher(IEnumerable<tbl_teacher_presonal_information> liteacher)
+        {
+            db.tbl_teacher_presonal_information.RemoveRange(liteacher);
+            return db.SaveChanges();
+        }
+
+        public int Updateteacher(tbl_teacher_presonal_information _teacher_p_i)
+        {
+            db.Entry(_teacher_p_i).State = System.Data.Entity.EntityState.Modified;
+            return db.SaveChanges();
+        }
+    }
+}
