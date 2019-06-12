e1faaba138347769573c0be9f6fa820dd246e811
masoomeshams <fmezadi9304265430@gmail.com>
2019-06-12 10:54:49 -0700

    مدل

--- Univercity/Univercity/Models/Admin/AdminRepository.cs
+++ Univercity/Univercity/Models/Admin/AdminRepository.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Univercity.Models;
+using Univercity.Models.Admin;
 
 namespace Univercity
 {
     public class AdminRepository : IAdmin
     {
         univercityEntities2 db = new univercityEntities2();
-        private tbl_teacher_presonal_information _teacher_p_i;
+
 
         public string code_national_teacher
         {
@@ -28,6 +29,12 @@ namespace Univercity
         {
             db.tbl_teacher_presonal_information.Remove(_teacher_p_i);
             return db.SaveChanges();
+
+        }
+
+        internal int insertteacher(tbl_teacher_presonal_information teacher)
+        {
+            throw new NotImplementedException();
         }
 
         public tbl_teacher_presonal_information FindTeacher(string code_national_teacher)
@@ -41,31 +48,36 @@ namespace Univercity
         {
             List<tbl_teacher_presonal_information> teachers = db.tbl_teacher_presonal_information.Where(c => c.code_national_teacher == code_national_teacher).ToList();
             return teachers;
+
         }
 
         public int insertlistteacher(IEnumerable<tbl_teacher_presonal_information> liteacher)
         {
+
             db.tbl_teacher_presonal_information.AddRange(liteacher);
             return db.SaveChanges();
+
         }
 
         public int Insertteacher(tbl_teacher_presonal_information _teacher_p_i)
         {
             db.tbl_teacher_presonal_information.Add(_teacher_p_i);
             return db.SaveChanges();
-            
+
         }
 
         public int removelistteacher(IEnumerable<tbl_teacher_presonal_information> liteacher)
         {
             db.tbl_teacher_presonal_information.RemoveRange(liteacher);
             return db.SaveChanges();
+
         }
 
         public int Updateteacher(tbl_teacher_presonal_information _teacher_p_i)
         {
             db.Entry(_teacher_p_i).State = System.Data.Entity.EntityState.Modified;
             return db.SaveChanges();
-        }
+
+        } 
     }
 }
