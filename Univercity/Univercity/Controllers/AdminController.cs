c486df34b099677dd49b29953cc5fec11ecee487
masoomeshams <fmezadi9304265430@gmail.com>
2019-06-12 10:53:40 -0700

    کنترلر

--- Univercity/Univercity/Controllers/AdminController.cs
+++ Univercity/Univercity/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Univercity.Models;
+using Univercity.Models.Admin;
 
 namespace Univercity.Controllers
 {
@@ -18,18 +19,31 @@ namespace Univercity.Controllers
 
         public ActionResult ForminsertAdmin()
         {
+
             return View();
         }
 
 
-        public ActionResult insertAdmin(tbl_teacher_presonal_information teacher)
-        {
-            return View();
-        }
-        public void insertteacher_i_p(tbl_teacher_presonal_information teacher)
+        //public ActionResult insertAdmin(tbl_teacher_presonal_information teacher)
+        //{
+        //    AdminRepository ar = new AdminRepository();
+        //    int result = ar.insertteacher(teacher);
+
+
+        //    if (result == 0)
+        //    {
+        //        // خظا
+        //    }
+        //    else
+        //    {
+        //        //درست
+        //    }
+        //    return View();
+        //}
+        public void insertteacher_p_i(tbl_teacher_presonal_information teacher)
         {
             AdminRepository ar = new AdminRepository();
-          int result=ar.Insertteacher(teacher);
+          int result=ar.insertteacher(teacher);
 
 
             if(result==0)
